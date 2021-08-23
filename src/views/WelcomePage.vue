6440a3a7d9ca92a222be32292387577d9250af34
care04 <carefarrar@gmail.com>
2021-08-23 12:15:24 -0400

    Finished Bob Spec One

---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Hello</h1>
+    <h2>Welcome to Open Learning Server a new way to learn.</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+})
+export default class Home extends Vue {}
+</script>
