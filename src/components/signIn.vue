7d8185d6a2473fdbf76ac8985ee5f90fc68e1d16
care04 <carefarrar@gmail.com>
2021-08-24 09:53:33 -0400

    added signIn component and used it on SignIn page

---
+++ src/components/signIn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>User Name</h4>
+    <input v-model="text" placeholder="user name"/>
+    <h4>Password</h4>
+    <input v-model="text" placeholder="password"/>
+    <h2></h2>
+    <router-link to="">
+      <button>Sign In</button>
+    </router-link>
+  </div>
+</template>
