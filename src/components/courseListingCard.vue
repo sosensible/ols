7b9dddd5c5eaa0b71721bfd84ffc95640372154f
care04 <carefarrar@gmail.com>
2021-08-24 13:24:52 -0400

    created course component added to course page

---
+++ src/components/courseListingCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3><em>Name:</em> {{ course.name }}</h3>
+    <p>Description: {{ course.shortDescription }}</p>
+    <h4>Price: {{ course.price }}</h4>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      course: {
+        name: "Course Name",
+        shortDescription: "Short Course Description",
+        price: 30,
+        id: 1,
+      },
+    };
+  },
+});
+</script>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h1>This is the courses page</h1>
+    <courseListingCard />
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import courseListingCard from "../components/courseListingCard.vue";
+
+@Component({
+  components: {
+    courseListingCard,
+  },
+})
+export default class Home extends Vue {}
+</script>
