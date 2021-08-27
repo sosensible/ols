55f655ba906b36ac566cb1c1721c4cac2fc22719
care04 <carefarrar@gmail.com>
2021-08-27 10:28:19 -0400

    added tests

---
+++ cypress/integration/George-Test/spec_2.js
@@ -0,0 +1,14 @@
+describe("George second spec test", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/my-courses");
+  });
+  it("has add a course button", () => {
+    cy.contains("Create New Course");
+  });
+  it("when button clicks goes to course creation page", () => {
+    cy.get("button").click();
+
+    cy.url().should("include", "/new-course");
+    cy.contains("New Course");
+  });
+});
