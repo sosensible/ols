85727b25e5bf63c9858c148180f767400864c50e
care04 <carefarrar@gmail.com>
2021-08-27 10:29:04 -0400

    added button and route to get to creation page

--- src/views/MyCourses.vue
+++ src/views/MyCourses.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <h1>This page is for courses that were created by this user</h1>
+    <router-link to="/new-course">
+      <button>Create New Course</button>
+    </router-link>
   </div>
 </template>
