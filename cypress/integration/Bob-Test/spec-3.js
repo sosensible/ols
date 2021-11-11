6eee77e5c83f5c669e32a4899241264aba66ea6b
care04 <carefarrar@gmail.com>
2021-11-11 12:09:46 -0500

    Bob_spec_3 finished refactoring

--- cypress/integration/Bob-Test/spec-3.js
+++ cypress/integration/Bob-Test/spec-3.js
@@ -1,23 +1,60 @@
+import { makeServer } from "../../../src/services/CourseService";
 describe("bob spec 3", () => {
+  let server;
+
+  const setup = () => {
+    server.create("course", {
+      id: 3,
+      name: "JavaScript",
+      shortDescription: "Short Description of JavaScript Course",
+      price: 39,
+      units: [
+        {
+          name: "What is JavaScript",
+          slug: "what_is_javascript",
+          lessons: [
+            {
+              name: "What is JavaScript Lesson1",
+              slug: "lesson_1",
+              content: "this is the content for what is JavaScript lesson 1",
+            },
+          ],
+        },
+      ],
+    });
+    server.create("course", {
+      id: 4,
+      name: "Bootstrap",
+      shortDescription: "Short Description of Bootstrap Course",
+      price: 39,
+      units: [
+        {
+          name: "What is Bootstrap",
+          slug: "what_is_bootstrap",
+          lessons: [
+            {
+              name: "what is bootstrap Lesson1",
+              slug: "lesson_1",
+              content: "this is the content for what is JavaScript lesson 1",
+            },
+          ],
+        },
+      ],
+    });
+  };
   beforeEach(() => {
-    cy.visit("http://localhost:8080/courses");
-    cy.get('[href="/course/1"] > div').click();
+    cy.visit("http://localhost:2803/courses");
+    server = makeServer({ environment: "test" });
+    setup();
+    cy.contains("Bootstrap").click();
+    cy.contains("What is Bootstrap").click();
   });
-  it("checks for lessons", () => {
-    //write code that checks for lessons
-    cy.get('.lessonArray')
+  it("opens a unit lesson", () => {
+    cy.contains("what is bootstrap Lesson1").click();
   });
-  it("click a lesson, route changes to course/lesson", () => {
-    //write code that chekcs for route change on lesson click
-    cy.get(':nth-child(4) > a > p')
-      .click()
-
-    cy.url().should("contain", "lesson");
-  });
-  it.only("checks that we are on course/lesson page", () => {
-    //write code to check we are on course lesson
-    cy.get(":nth-child(4) > a > p").click();
-
-    cy.contains("This Lessons Name")
+  it("has the lesson information on lesson page", () => {
+    cy.contains("what is bootstrap Lesson1").click();
+    cy.contains("what is bootstrap Lesson1");
+    cy.contains("this is the content for what is JavaScript lesson 1");
   });
 });
