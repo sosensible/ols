dcd7a5f17c4b6a18ad32f418bee6a1ffac362820
care04 <carefarrar@gmail.com>
2021-11-11 11:54:42 -0500

    bob_spec_2 test finished

--- cypress/integration/Bob-Test/spec-2.js
+++ cypress/integration/Bob-Test/spec-2.js
@@ -2,8 +2,48 @@ import { makeServer } from "../../../src/services/CourseService";
 
 describe("bob spec 2 tests", () => {
   let server;
+
+  const setup = () => {
+    server.create("course", {
+      id: 3,
+      name: "JavaScript",
+      shortDescription: "Short Description of JavaScript Course",
+      price: 39,
+      units: [
+        {
+          name: "What is JavaScript",
+          slug: "what_is_javascript",
+          lessons: [
+            {
+              name: "What is JavaScript Lesson1",
+              slug: "lesson_1",
+            },
+          ],
+        },
+      ],
+    });
+    server.create("course", {
+      id: 4,
+      name: "Bootstrap",
+      shortDescription: "Short Description of Bootstrap Course",
+      price: 39,
+      units: [
+        {
+          name: "What is Bootstrap",
+          slug: "what_is_bootstrap",
+          lessons: [
+            {
+              name: "what is bootstrap Lesson1",
+              slug: "lesson_1",
+            },
+          ],
+        },
+      ],
+    });
+  };
   beforeEach(() => {
     server = makeServer({ environment: "test" });
+    setup();
     cy.visit("http://localhost:2803");
   });
 
@@ -11,12 +51,21 @@ describe("bob spec 2 tests", () => {
     server.shutdown();
   });
   it("shows the courses from server", () => {
-    server.create("course", { id: 3, name: "JavaScript" });
-    server.create("course", { id: 4, name: "Bootstrap" });
-
     cy.contains("Courses").click();
 
     cy.contains("Bootstrap");
     cy.contains("JavaScript");
+  });
+  it("goes to correct link when course clicked", () => {
+    cy.contains("Courses").click();
+
+    cy.contains("Bootstrap").click();
+    cy.url().should("include", "/4");
+  });
+  it("course detail page shows course title", () => {
+    cy.contains("Courses").click();
+    cy.contains("Bootstrap").click();
+    cy.url().should("include", "/4");
+    cy.contains("Bootstrap");
   });
 });
