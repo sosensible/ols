600acb5d0035714d06198a3320041dcb991eb63d
care04 <carefarrar@gmail.com>
2021-11-23 12:15:28 -0500

    added slug to the unit creation

--- src/views/course/AddUnit.vue
+++ src/views/course/AddUnit.vue
@@ -12,6 +12,16 @@
       />
       <label for="unitName">Unit Name</label>
     </div>
+    <div class="form-floating mb-3">
+      <input
+        id="unitSlug"
+        class="form-control"
+        type="text"
+        v-model="unit.slug"
+        data-testid="unitSlugField"
+      />
+      <label for="unitSlug">Unit Slug</label>
+    </div>
     <button @click="saveUnit" data-testid="saveButton">Save Unit</button>
   </div>
 </template>
@@ -31,7 +41,7 @@ export default {
   methods: {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     saveUnit() {
-      if (this.unit.name != "") {
+      if (this.unit.name != "" && this.unit.slug != "") {
         this.course.units.push(this.unit);
         this.$store.dispatch("updateCourse", this.course);
         this.$router.push("/course/" + this.course.id);
